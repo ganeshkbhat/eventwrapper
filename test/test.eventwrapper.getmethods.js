495e31bf17c032a8cc58b459310a03162052e0fb
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-09-10 16:02:01 +0000

    [TEMP] Adding wrapMethods functions to extend your function with wrappers. Adding file for demo of class based extend

---
+++ test/test.eventwrapper.getmethods.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const EventEmitter = require('events');
+
+const MyFunctionWithEvents = require("../src/index");
+
+describe('Manipulating functions using getMethods function by extending classes', () => {
+    describe('instance', () => {
+        it('should getmethods with event emitters', () => {
+            
+        });
+    });
+});
